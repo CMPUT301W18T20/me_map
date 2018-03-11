64e7fd745025acd681fcd6bc4779b431fc38cd57
Jolene Poulin <jcpoulin@ualberta.ca>
2018-03-11 16:37:33 -0600

    Inline map works for one location request

--- app/src/main/java/com/example/jpoulin/memap/MapsActivity.java
+++ app/src/main/java/com/example/jpoulin/memap/MapsActivity.java
@@ -1,10 +1,16 @@
 package com.example.jpoulin.memap;
 
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,10 +23,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String location_string;
+    private EditText location;
+    private Timer t = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
+        Button send = findViewById(R.id.button);
+
+        ScheduledThreadPoolExecutor exec = new ScheduledThreadPoolExecutor(1);
+        exec.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                updateLocation();
+            }
+        }, 0, 1, TimeUnit.SECONDS); // execute every 60 seconds
+
+    }
+
+
+    public void updateLocation() {
+        Button send = findViewById(R.id.button);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                Log.e("Button did click", "yes");
+                location = findViewById(R.id.location);
+                location_string = location.getText().toString();
+                setGlobalLocation(location_string);
+                Log.e("Location string", location_string);
+                Location location = new Location(location_string);
+
+            }
+        });
     }
 
 
@@ -74,17 +115,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         assert addresses != null;
-        Address current_address = addresses.get(0);
+        Address current_address;
+        try {
+            current_address = addresses.get(0);
 
-        Double lat = current_address.getLatitude();
-        Double lon = current_address.getLongitude();
+            Double lat = current_address.getLatitude();
+            Double lon = current_address.getLongitude();
 
 
-        // Add a marker in the user provided location and move the camera
-        LatLng display_location = new LatLng(lat, lon);
-        float zoomLevel = 12.0f;
-        mMap.addMarker(new MarkerOptions().position(display_location).title("Marker at task location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(display_location, zoomLevel));
+            // Add a marker in the user provided location and move the camera
+            LatLng display_location = new LatLng(lat, lon);
+            float zoomLevel = 12.0f;
+            mMap.addMarker(new MarkerOptions().position(display_location).title("Marker at task location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(display_location, zoomLevel));
+
+        } catch (IndexOutOfBoundsException e) {
+            Toast.makeText(this, "Invalid location!", Toast.LENGTH_LONG).show();
+
+        }
+
+        updateLocation();
+
+    }
+
+    public void setGlobalLocation(String location) {
+        ((GlobalVariables) this.getApplication()).setLocation(location);
     }
 
 }
